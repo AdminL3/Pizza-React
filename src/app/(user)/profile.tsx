40e3522733b87938e0df561ede7738c8c9b3ad27
AdminL3 <l-blu@outlook.de>
2024-07-07 17:59:25 +0200

    Profile page + orders +settings

--- src/app/(user)/profile.tsx
+++ src/app/(user)/profile.tsx
@@ -1,11 +1,21 @@
+import { Link, Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Profile Two</Text>
+      <Stack.Screen options={{ headerTitle: 'Profile', headerRight: () => null }} />
+      <View style={styles.box}>
+        <Link href="pages/orders" style={styles.title}>Orders</Link>
+      </View>
+      <View style={styles.box}>
+        <Link href="pages/settings" style={styles.title}>Settings</Link>
+      </View>
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +26,17 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'white',
+  },
+  box: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '70%',
+    height: 60,
+    backgroundColor: '#161622',
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: 'black',
+    marginVertical: 10,
   },
 });
