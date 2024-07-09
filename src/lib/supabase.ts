e7e22b1d72c9dd0312121d1cdfb0f96f71d6b93a
AdminL3 <l-blu@outlook.de>
2024-07-09 15:17:39 +0200

    Added supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+    getItem: (key: string) => {
+        return SecureStore.getItemAsync(key);
+    },
+    setItem: (key: string, value: string) => {
+        SecureStore.setItemAsync(key, value);
+    },
+    removeItem: (key: string) => {
+        SecureStore.deleteItemAsync(key);
+    },
+};
+
+const supabaseUrl = 'https://dnllvxbuayticibhqdjn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRubGx2eGJ1YXl0aWNpYmhxZGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1MjI0NzgsImV4cCI6MjAzNjA5ODQ3OH0.UVDXokAmpbNrhWUIHEEouxQjZM-6QdWTIUthbUwUyao';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: ExpoSecureStoreAdapter as any,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
